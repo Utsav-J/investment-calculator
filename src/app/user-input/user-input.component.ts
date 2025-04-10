85cda6c543d3431cd9db0c04d69056687004ae07
UtsavJ <utsavjaiswal2004@gmail.com>
2025-04-10 14:49:23 +0530

    initial commit form input

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredDuration = '5';
+  enteredExpectedReturns = '10';
+
+  onSubmit(){
+    console.log("Submitted");
+  }
+}
